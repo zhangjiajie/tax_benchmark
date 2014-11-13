3fe9ba1c91d1271ae9eebf0b84418f3be68f38d5
zhangjiajie <bestzhangjiajie@gmail.com>
2014-11-13 12:25:57 +0100

    general tester

--- script/mis_tests.py
+++ script/mis_tests.py
@@ -3,6 +3,7 @@ import os
 import sys
 import random
 import math
+import time
 from subprocess import call
 from ete2 import SeqGroup
 
@@ -18,10 +19,46 @@ def rank2string(l):
     s = s[0:-1]
     return s
 
-def findmis(refseq, reftax, name, method, temfolder):
-    fq = open(temfolder + "query.fa", "w")
+
+def is_mislabel(orig_ranks, ranks):
+    EMPTY_RANK = "-"   # change it if needed
+    mislabel_lvl = -1
+    min_len = min(len(orig_ranks),len(ranks))
+    for rank_lvl in range(min_len):
+        if ranks[rank_lvl] != EMPTY_RANK and ranks[rank_lvl] != orig_ranks[rank_lvl]:
+            mislabel_lvl = rank_lvl
+            break
     
-    with open(temfolder + "seqs.fa", "w") as fo:
+    rk_name = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
+    
+    
+    if mislabel_lvl >= 0:
+        #       flag  MislabeledLevel       OriginalLabel              ProposedLabel   
+        return True, rk_name[mislabel_lvl], orig_ranks[mislabel_lvl], ranks[mislabel_lvl]
+    else:
+        return False, None, None, None
+
+#;SeqID  MislabeledLevel OriginalLabel   ProposedLabel   Confidence      OriginalTaxonomyPath    ProposedTaxonomyPath    PerRankConfidence
+
+
+def findmis(refseq, reftax, name, old_tax, method, foutput):
+    """
+    refseq: full list of sequences, unaligned
+    reftax: full list of taxonomy
+    name: taxa name you want to check for errors
+    old_tax: taxonomy before the test, a list
+    """
+    basepath = os.path.dirname(os.path.abspath(__file__))
+    tmpfolder = basepath + "/tmp/"
+    tmpname = tmpfolder + str(time.time())
+    
+    #below generate tmp files
+    fquery = tmpname + "query.fa"
+    frefseq = tmpname + "seqs.fa"
+    frefrank = tmpname + "ranks.tax"
+    
+    fq = open(fquery, "w")
+    with open(frefseq, "w") as fo:
         for ele in refseq:
             if ele[0] != name:
                 fo.write(">" + ele[0] + "\n")
@@ -30,24 +67,31 @@ def findmis(refseq, reftax, name, method, temfolder):
                 fq.write(">" + ele[0] + "\n")
                 fq.write(ele[1] + "\n")
     
-    with open(temfolder + "ranks.tax", "w") as fo:
+    with open(frefrank, "w") as fo:
         for ele in reftax:
             if ele[0] != name:
                 fo.write(ele[0] + "	" + rank2string(ele[1]) + "\n")
-    
     fq.close()
     
-    run(query = temfolder + "query.fa" , refseq = temfolder + "seqs.fa", taxonomy = temfolder + "ranks.tax", method = method, outdir = temfolder)
-    results = temfolder + "query_tax_assignments.txt"
+    #below do the test
+    run(query = fquery , refseq = frefseq, taxonomy = frefrank, method = method, outdir = tmpfolder)
+    results = tmpfolder + "query_tax_assignments.txt"
     
     resultss = ""
     with open(results) as fo:
         resultss = fo.readline()
     
-    os.remove(temfolder + "query.fa")
-    os.remove(temfolder + "seqs.fa")
-    os.remove(temfolder + "ranks.tax")
-    os.remove(temfolder + "query_tax_assignments.txt")
+    os.remove(fquery)
+    os.remove(frefseq)
+    os.remove(frefrank)
+    os.remove(tmpfolder + "query_tax_assignments.txt")
+    
+    flag, MislabeledLevel, OriginalLabel, ProposedLabel = is_mislabel(old_tax, resultss.split()[1].split(";"))
+    
+    if flag:
+        #;SeqID  MislabeledLevel OriginalLabel   ProposedLabel   Confidence      OriginalTaxonomyPath    ProposedTaxonomyPath    PerRankConfidence
+        with open(foutput, "a+") as fo:
+            fo.write(name + "    " + MislabeledLevel + "    " + OriginalLabel + "    " + ProposedLabel  + "    " + "1    " +  rank2string(old_tax) + "    " + resultss.split()[1] + "    ;"\n")
     
     return resultss, resultss.split()[1].split(";")
     
@@ -129,16 +173,55 @@ def autotest(refseq, reftax, testingtax, tf = "/home/zhangje/GIT/tax_benchmark/s
         fo.write("uclust"+ "   " +repr(num_corrected_uclust) + " " + repr(num_unchanged_uclust) + "\n")
         fo.write("rdp"+ "   " +repr(num_corrected_rdp) + " " + repr(num_unchanged_rdp) + "\n")
         fo.write("blast"+ "   " +repr(num_corrected_blast) + " " + repr(num_unchanged_blast) + "\n")
-     
+
+
+def curator(refseq, reftax, method, output, testingtax = ""):
+    seqs = SeqGroup(refseq)
+    ranks = []
+    with open(reftax) as fo:
+        for line in fo:
+            ll = line.split()
+            ele = [ll[0], ll[1].split(";")]
+            ranks.append(ele)
+    testings = []
+    if testingtax!="":
+        with open(testingtax) as fo:
+            for line in fo:
+                ll = line.split()
+                ele = [ll[0], ll[1].split(";")]
+                testings.append(ele)
+    else:
+        testings = ranks
+    
+    for test in testings:
+        #refseq, reftax, name, old_tax, method, foutput
+        ru, result_string = findmis(refseq = seqs, reftax = ranks, name = test[0], method = method, foutput = output)
+        print(result_string)
+    
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3: 
-        print("python mis_tests.py /home/zhangje/GIT/tax_benchmark/simulation_LTP/sim.fasta /home/zhangje/GIT/tax_benchmark/simulation_LTP/mislable/5/mLTP1.tax /home/zhangje/GIT/tax_benchmark/simulation_LTP/mislable/5/mLTP1.true.tax  /home/zhangje/GIT/tax_benchmark/script/tmp/")
+        print("""python mis_tests.py 
+        /home/zhangje/GIT/tax_benchmark/simulation_LTP/sim.fasta 
+        /home/zhangje/GIT/tax_benchmark/simulation_LTP/mislable/5/mLTP1.tax  
+        uclust blast rdp
+        /home/zhangje/GIT/tax_benchmark/simulation_LTP/mislable/5/mLTP1.tax.uclust.out
+        """)
+        
+        print("seqs taxonomy method output [testtax]")
+        
         sys.exit()
     
-    autotest(refseq = sys.argv[1], 
-    reftax = sys.argv[2], 
-    testingtax = sys.argv[3],
-    tf = sys.argv[4])
+    if len(sys.argv) > 4:
+        curator(refseq = sys.argv[1], reftax = sys.argv[2], method = sys.argv[3], output = sys.argv[4], testingtax = sys.argv[5])
+    else:
+        curator(refseq = sys.argv[1], reftax = sys.argv[2], method = sys.argv[3], output = sys.argv[4])
+    
+    #autotest(refseq = sys.argv[1], 
+    #reftax = sys.argv[2], 
+    #testingtax = sys.argv[3],
+    #tf = sys.argv[4])
     
     
     
